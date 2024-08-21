7b7f62384dc95ffeb8162c29346a537118ea03d0
Yan Moroz <yanchillosthegreat@yandex.ru>
2024-08-22 01:14:07 +0300

    first commit

--- Package.swift
+++ Package.swift
@@ -9,15 +9,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CocoaDebugSwift",
-            targets: ["CocoaDebugSwift"]),
+            targets: ["CocoaDebugSwift"]
+        )
+    ],
+    dependencies: [
+      .package(
+        url: "https://github.com/yanmoroz/CocoaDebugObjc.git",
+        branch: "main"
+      )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CocoaDebugSwift"),
-        .testTarget(
-            name: "CocoaDebugSwiftTests",
-            dependencies: ["CocoaDebugSwift"]),
+            name: "CocoaDebugSwift",
+            dependencies: ["CocoaDebugObjc"]
+        ),
     ]
 )
