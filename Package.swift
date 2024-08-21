26f9c486de685689f2880bed57c0213b417a0d63
Yan Moroz <yanchillosthegreat@yandex.ru>
2024-08-22 01:57:19 +0300

    Add Resources folder

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CocoaDebugSwift",
-            dependencies: ["CocoaDebugObjc"]
+            dependencies: ["CocoaDebugObjc"],
+            resources: [
+                .process("Resources")
+            ]
         ),
     ]
 )
